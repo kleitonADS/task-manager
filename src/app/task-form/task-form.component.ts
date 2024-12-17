d78be76a959194a741b64ca47ab3fbac58d7da1e
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2024-12-16 23:45:20 -0300

    Componente de formulário para adicionar e editar tarefas

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.css'
+  styleUrls: ['./task-form.component.css']
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  task = { titulo: '', descricao: '', status: 'pendente' };
+  isEdit = false;
 
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const taskId = this.route.snapshot.paramMap.get('id');
+    if (taskId) {
+      this.isEdit = true;
+      this.taskService.getTask(taskId).subscribe(task => {
+        this.task = task;
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.isEdit) {
+      this.taskService.updateTask(this.task).subscribe(() => {
+        this.router.navigate(['/']);
+      });
+    } else {
+      this.taskService.createTask(this.task).subscribe(() => {
+        this.router.navigate(['/']);
+      });
+    }
+  }
 }
