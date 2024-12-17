45a34c47f5c8a17e4b37b2fb1d449a5a5d7cc9dd
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2024-12-16 23:46:20 -0300

    Serviço para interação com a API de tarefas

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:8001/api/tarefas'; // URL da API Laravel
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getTask(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: any): Observable<any> {
+    return this.http.post(this.apiUrl, task);
+  }
+
+  updateTask(task: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${task.id}`, task);
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
